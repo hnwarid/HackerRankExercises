109891136abdbbde4c58d29bebb4b96f6bcea105
Hamzah Nurwarid <hn.warid@gmail.com>
2022-03-26 21:04:55 +0700

    first python commit

---
+++ Python/30DaysofCode/Day 1 Data Types.py
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+int_input = int(input())
+dbl_input = float(input())
+str_input = str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + int_input)
+# Print the sum of the double variables on a new line.
+print(d +dbl_input)
+# Concatenate and print the String variables on a new line
+print(s + str_input)
+# The 's' variable above should be printed first.
